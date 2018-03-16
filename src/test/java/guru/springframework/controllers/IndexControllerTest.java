bfb38c421c40e7bd6a456e9503265b10eaf74edb
Jackie <Jackie@192.168.0.11>
2018-03-16 14:53:02 +0000

    Adding mockMVC to controller class.

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,17 +1,27 @@
 package guru.springframework.controllers;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 
 public class IndexControllerTest {
@@ -30,15 +40,44 @@ public class IndexControllerTest {
 		MockitoAnnotations.initMocks(this);
 		indexController = new IndexController(recipeService);
 	}
+	
+	@Test
+	public void testMockMVC() throws Exception {
+		
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("index"));
+		
+	}
 
  	@Test
 	public void getIndexPage() {
-
+ 		
+ 		// Given	
+ 		Recipe recipe1 = new Recipe();
+ 		recipe1.setId(1L);
+ 		
+ 		Recipe recipe2 = new Recipe();
+ 		recipe2.setId(2L);
+ 		
+ 		Set<Recipe> recipes = new HashSet<Recipe>();
+ 		recipes.add(recipe1);
+ 		recipes.add(recipe2);
+ 		
+ 		when(recipeService.getRecipes()).thenReturn(recipes);
+ 		
+ 		@SuppressWarnings("unchecked")
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+ 		
+ 		// When
  		String viewName = indexController.getIndexPage(model);
  		
+ 		// Then
  		assertEquals("index", viewName);
  		verify(recipeService, times(1)).getRecipes();
- 		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+ 		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+ 		Set<Recipe> setInController = argumentCaptor.getValue();
+ 		assertEquals(2, setInController.size());
 	}
-
 }
